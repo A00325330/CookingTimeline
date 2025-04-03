701fda152ec1e0d15624711be5317b914fd65e6f
AaronR23 <aaron2612@yahoo.ie>
2025-04-03 17:50:09 +0100

    Uncommented out tests

--- Individual_proj/src/test/java/com/tus/group_project/karate/RecipeKarateTest.java
+++ Individual_proj/src/test/java/com/tus/group_project/karate/RecipeKarateTest.java
@@ -27,13 +27,13 @@ public class RecipeKarateTest {
 	}
 
 
-//	@Karate.Test
-//	Karate runRecipeTests() {
-//		return Karate.run(
-//				"classpath:features/recipes/createRecipe.feature",
-//				"classpath:features/recipes/getMyRecipes.feature", 
-//				"classpath:features/recipes/getRecipeById.feature",
-//				"classpath:features/recipes/getPublicRecipes.feature"
-//		).relativeTo(getClass());
-//	}
+	@Karate.Test
+	Karate runRecipeTests() {
+		return Karate.run(
+				"classpath:features/recipes/createRecipe.feature",
+				"classpath:features/recipes/getMyRecipes.feature", 
+				"classpath:features/recipes/getRecipeById.feature",
+				"classpath:features/recipes/getPublicRecipes.feature"
+		).relativeTo(getClass());
+	}
 }
