1f2fd7b12f792c74f3a17ea283ebe6458f5d7539
AaronR23 <aaron2612@yahoo.ie>
2025-04-03 18:58:12 +0100

    NGrok test

--- Individual_proj/src/main/java/com/tus/group_project/GroupProjectApplication.java
+++ Individual_proj/src/main/java/com/tus/group_project/GroupProjectApplication.java
@@ -3,7 +3,7 @@ package com.tus.group_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+// Ngrok test
 @SpringBootApplication
 public class GroupProjectApplication {
 
